0b45c8e0636a4468e227278642940828274367b9
Alal11 <spalqj56@naver.com>
2023-11-07 14:59:17 +0900

    완전 초기 세팅

---
+++ src/main/java/com/example/java_teample/JavaTeampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_teample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTeampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaTeampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/java_teample/JavaTeampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_teample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTeampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
